dfc44bb9496ef57fe73b54976bb2c3da87301e8d
Guillaume Denier <guillaume_denier@hotmail.fr>
2022-11-02 17:38:36 +0100

    add tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./styles/app.css"
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }]
+}
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -16,7 +21,7 @@ export const meta: MetaFunction = () => ({
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="fr">
       <head>
         <Meta />
         <Links />
